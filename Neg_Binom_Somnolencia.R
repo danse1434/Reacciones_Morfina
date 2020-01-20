eca83565319f9c1cda0f7d2a4d144ea6cb21a5d0
kdcloop <dsparrag@unal.edu.co>
2020-01-20 00:26:50 -0500

    Cambios en los cálculos con regresión negativa binomial.

--- Neg_Binom_Somnolencia.R
+++ Neg_Binom_Somnolencia.R
@@ -7,71 +7,129 @@ require('grid')
 vplayout <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
 require('betareg')
 
-# Librería Predeterminada
+##########################################################################-
+# Introducción ------------------------------------------------------------
+##########################################################################-
+
 setwd(file.path('F:','Documentos','Estudio - Documentos',
                 'Estadistica_2013-2','Regresion','7. Modelos GLM',
                 'Binominal','Reacciones_Morfina'))
 
-df1 <- read.csv(file = './Datos/Somnolencia.csv', header = T, sep = ',', dec = '.')
+df1 <- read.csv(file = './Datos/Somnolencia.csv', header = T, sep = ',', 
+                dec = '.')
 
-# Regresión Beta
-br1 <- betareg(formula = Respuesta ~ Dosis, 
-               data = df1[df1[,'Farmaco']== 'Meperidina',])
+##########################################################################-
+# Regresión Beta ---------------------------------------------------------- 
+##########################################################################-
+
+br1 <- df1 %>% 
+  filter(Farmaco == 'Meperidina') %>% 
+  betareg(formula = Respuesta ~ Dosis, 
+               data = .)
+
+br2 <- df1 %>% 
+  filter(Farmaco == 'Morfina') %>% 
+  betareg(formula = Respuesta ~ Dosis, 
+          data = .)
 
 
-br2 <- betareg(formula = Respuesta ~ Dosis, 
-               data = df1[df1[,'Farmaco']== 'Morfina',])
-
+##########################################################################-
+# Predicción Modelo -------------------------------------------------------
+##########################################################################-
 # Datos Nuevos
-newdata2 <- with(df1, data.frame(Dosis = rep(seq(from = 0.07, to = 4.0, by = 0.01),2)))
-newdata2[,'Farmaco'] = rep(x = c('Meperidina', 'Morfina'), each = dim(newdata2)[1]/2)
+newdata2 <- data.frame(Dosis = rep(seq(from = 0.01, to = 4.0, by = 0.01),2))
+newdata2$Farmaco = rep(x = c('Meperidina', 'Morfina'), 
+                       each = dim(newdata2)[1]/2)
 
 # Predicciones Regresión Beta
-for(i in 1:dim(newdata2)[1]){
-  if (newdata2[i,'Farmaco'] == 'Meperidina') {
-    newdata2[i,'fit'] = predict(br1, newdata = newdata2[i,], type = 'response')
-  } else {
-    newdata2[i,'fit'] = predict(br2, newdata = newdata2[i,], type = 'response')
-  }
-}
+newdata2 <- newdata2 %>% 
+  mutate(Fit = case_when(
+    Farmaco == 'Meperidina' ~ predict(br1, newdata = newdata2, 
+                                      type = 'response'),
+    Farmaco == 'Morfina' ~ predict(br2, newdata = newdata2, 
+                                   type = 'response'),
+    TRUE ~ NA_real_
+  )) %>% 
+  mutate(se.fit = case_when(
+    Farmaco == 'Meperidina' ~ predict(br1, newdata = newdata2, 
+                                      type = 'variance'),
+    Farmaco == 'Morfina' ~ predict(br2, newdata = newdata2, 
+                                   type = 'variance'),
+    TRUE ~ NA_real_
+  ))
 
-# Desviación Estandar Regresión Beta
-for(i in 1:dim(newdata2)[1]){
-  if (newdata2[i,'Farmaco'] == 'Meperidina') {
-    newdata2[i,'se.fit'] = predict(br1, newdata = newdata2[i,], type = 'variance') %>% sqrt(.)
-  } else {
-    newdata2[i,'se.fit'] = predict(br2, newdata = newdata2[i,], type = 'variance') %>% sqrt(.)
-  }
-}
+newdata2 <- newdata2 %>% 
+  mutate(LI = case_when(
+    Farmaco == 'Meperidina' ~ predict(br1, newdata = newdata2, 
+                                      type = 'quantile', at = c(0.025)), 
+    Farmaco == 'Morfina' ~ predict(br2, newdata = newdata2, 
+                                   type = 'quantile', at = c(0.025)), 
+    TRUE ~ NA_real_
+  )) %>%
+  mutate(LS = case_when(
+    Farmaco == 'Meperidina' ~ predict(br1, newdata = newdata2, 
+                                      type = 'quantile', at = c(0.975)), 
+    Farmaco == 'Morfina' ~ predict(br2, newdata = newdata2, 
+                                   type = 'quantile', at = c(0.975)), 
+    TRUE ~ NA_real_
+  ))
 
-# Gráfico de Puntos
+##########################################################################-
+# Gráfico de Puntos  ------------------------------------------------------
+##########################################################################-
 # Morfina - Gráfico
-# GA <- 
-  ggplot() + 
-  geom_point(data = df1[df1[,'Farmaco']=='Morfina',], 
-             aes(x = Dosis, y = Respuesta, group = Farmaco),shape = 8, col = 'red2') + 
-  geom_line(data=newdata2[newdata2[,'Farmaco']=='Morfina',], 
-            aes(x = Dosis, y = fit, group = Farmaco), col = 'red2') +
-  scale_x_continuous(breaks = seq(0.05,0.6,0.1)) +
+GA <- df1 %>% 
+  ggplot(mapping = aes(x = Dosis, y = Respuesta, group = Farmaco, 
+                       col = Farmaco), 
+         shape = 8) + 
+  geom_ribbon(data = newdata2, aes(x = Dosis, ymin = LI, ymax = LS, 
+                                   fill = Farmaco), alpha = 0.1,
+              inherit.aes = F) +
+  geom_point() +
+  geom_line(data=newdata2, aes(y = Fit)) +
   scale_y_continuous(breaks = seq(0,1,0.2)) +
   labs(x = 'Dosis (mg/kg)', y = 'Somnolencia')+
-  annotate(geom = 'label', x = 0.5, y = 0.2, label = 'Morfina') +
   theme_classic() +
-  theme(panel.border = element_rect(colour = 'black', fill = NA))+
-  coord_cartesian(xlim = c(0.05,0.6)) 
+  facet_grid(.~Farmaco) +
+  theme(panel.border = element_rect(colour = 'black', fill = NA),
+        legend.position = 'none') 
+  
 
-# Meperidina - Gráfico
-# GB <- 
-  ggplot() + 
-  geom_point(data = df1[df1[,'Farmaco']=='Meperidina',], 
-             aes(x = Dosis, y = Respuesta, group = Farmaco),shape = 8, col = 'blue3') + 
-  geom_line(data=newdata2[newdata2[,'Farmaco']=='Meperidina',], 
-            aes(x = Dosis, y = fit, group = Farmaco), col = 'blue3') +
-  labs(x = 'Dosis (mg/kg)', y = 'Somnolencia') +
-  scale_x_continuous(breaks = seq(0.00,3.5,0.5)) +
+
+
+newdata3 <- newdata2 %>% 
+  filter(Farmaco == 'Morfina' & Dosis < 0.7 | 
+           Farmaco == 'Meperidina')
+
+
+GB <- 
+  df1 %>% 
+  ggplot(mapping = aes(x = Dosis, y = Respuesta, group = Farmaco, 
+                       col = Farmaco), 
+         shape = 8) + 
+  geom_ribbon(data = newdata3, aes(x = Dosis, ymin = LI, ymax = LS, 
+                                   fill = Farmaco), alpha = 0.1,
+              inherit.aes = F) +
+  geom_point() +
+  geom_line(data=newdata3, aes(y = Fit)) +
   scale_y_continuous(breaks = seq(0,1,0.2)) +
-  theme_classic() + 
-  annotate(geom = 'label', x = 2.5, y = 0.2, label = 'Meperidina') +
-  theme(panel.border = element_rect(colour = 'black', fill = NA))+
-  coord_cartesian(xlim = c(0.3,3.5)) 
+  labs(x = 'Dosis (mg/kg)', y = 'Somnolencia')+
+  theme_classic() +
+  facet_grid(.~Farmaco, scales = 'free_x') +
+  theme(panel.border = element_rect(colour = 'black', fill = NA),
+        legend.position = 'none') 
 
+
+ggsave(filename = './Figuras/Somnolencia_NB.pdf', device = 'pdf',
+       plot = GB,
+       width = 7,height = 4, units = 'in')  
+  
+# df1 %>% 
+#   group_by(Farmaco) %>% 
+#   summarise(min=min(Dosis),
+#             max=max(Dosis))
+  
+  
+
+  
+  
